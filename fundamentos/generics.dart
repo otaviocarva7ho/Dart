e045c952621d55b7880e2d7d48b5dd8d7a4ea3fc
Otávio Carvalho <carvalho07otavioh@gmail.com>
2025-09-29 20:48:08 -0300

    Generics

---
+++ fundamentos/generics.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  List <String> frutas = ['Banana', 'Maça', 'Laranja'];
+  frutas.add('Melão');
+
+  print(frutas);
+
+  Map <String, double> salarios = {
+    'gerente': 19345.78,
+    'vendedor': 16345.80,
+    'estagiário': 600.00,
+  };
+
+  print(salarios);
+}
