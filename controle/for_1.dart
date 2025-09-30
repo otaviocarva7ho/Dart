79885be4e7795feb5dae1b315b680a06a2d05125
Otávio Carvalho <carvalho07otavioh@gmail.com>
2025-09-30 13:25:39 -0300

    For #02

--- controle/for_1.dart
+++ controle/for_1.dart
@@ -14,5 +14,10 @@ void main(List<String> args) {
 
   print("[FORA] b = $b");
 
+  var notas = [8.9, 9.3, 7.8, 6.9, 9.1];
+  for (var i = 0; i < notas.length; i++) {
+    print("Nota ${i + 1} = ${notas[i]}");
+  }
+  
   print("Fim!");
 }
