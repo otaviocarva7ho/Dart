68afa23ca83f33457188bb53a397200f0efc635d
Otávio Carvalho <carvalho07otavioh@gmail.com>
2025-09-30 11:43:49 -0300

    Desafio If/Else

---
+++ controle/desafio_if_else.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  var nota = 0.3;
+
+  if (nota >= 9.0) // ; NÃO USA ; EM ESTRUTURAS DE CONTROLE * 
+  {
+    print("Parabéns! Vc foi brilhante!");
+  }
+
+  // * EXCEÇÃO DO/WHILE!!!
+}
