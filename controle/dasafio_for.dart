0ce52c98fed7304b27a0576ff6cb2da991ea0a9c
Otávio Carvalho <carvalho07otavioh@gmail.com>
2025-10-01 13:20:16 -0300

    Desafio For

---
+++ controle/dasafio_for.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  // var valor = '#';
+  // for (var i = 0; i < 6; i++) {
+  //   print(valor);
+  //   valor += '#';
+  // }
+
+  for(var valor = '#'; valor != '#######'; valor += '#'){
+    print(valor);
+  }
+}
