f11cebe8f30ad985dd191fe5d99d31402aa5e62f
Otávio Carvalho <carvalho07otavioh@gmail.com>
2025-10-01 11:57:35 -0300

    Break / Continue

---
+++ controle/break_continue.dart
@@ -0,0 +1,19 @@
+void main(List<String> args) {
+  for (var i = 0; i < 10; i++) {
+    if (i == 6) {
+      break;
+    }
+    print(i);
+  }
+
+  print('Depois do laço for #01');
+
+  for (var i = 0; i < 10; i++) {
+    if (i % 2 == 0) {
+      continue;
+    }
+    print(i);
+  }
+
+  print('Depois do laço for #02');
+}
